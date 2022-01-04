b0f4246d5c36be504df62d7192025aa71b2ebe26
kenaszogara <kenaszogara@live.com>
2022-01-04 18:08:29 +0800

    add logo

--- pages/_app.js
+++ pages/_app.js
@@ -17,6 +17,11 @@ function MyApp({ Component, pageProps }) {
       <DefaultSeo {...SEO} />
       <Head>
         <link
+          rel="icon"
+          href="/images/pixil-frame-0.png"
+          type="image/png"
+        ></link>
+        <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
           integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ=="
